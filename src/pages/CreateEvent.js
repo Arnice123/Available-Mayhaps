dd21018b526719409245ffa0e2fe3abc65ffb366
Arya Akhavein <aryaakhavein92@gmail.com>
2025-05-02 12:00:20 -0700

    Added missing functions to create event

--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -5,6 +5,9 @@ import { useParams, useNavigate } from 'react-router-dom';
 export default function CreateEvent() {
     const { groupId } = useParams();
     const [availability, setAvailability] = useState({})
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const navigate = useNavigate()
 
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
     const times = ['12am', '1am','2am', '3am','4am', '5am', '6am', '7am','8am', '9am', '10am', '11am', '12pm', '1pm','2pm', '3pm','4pm', '5pm', '6pm', '7pm','8pm', '9pm', '10pm', '11pm',]
@@ -15,6 +18,10 @@ export default function CreateEvent() {
           ...prev,
           [key]: !prev[key]
         }));
+    }
+
+    async function handleSubmit(e) {
+        
     }
 
     return (
