5477b0b27cd546f16e71f11e36c9a38c900cd0b1
Arya Akhavein <aryaakhavein92@gmail.com>
2025-05-06 12:45:50 -0700

    added missing imports

--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { format, addDays, eachDayOfInterval } from 'date-fns';
+import { format, parseISO, eachDayOfInterval } from 'date-fns';
 
 export default function CreateEvent() {
   const { groupId } = useParams();
